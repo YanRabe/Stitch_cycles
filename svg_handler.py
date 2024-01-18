d92caf3a09bccad61602380daaba55d013e997ea
Freud69 <mcnecron@gmail.com>
2024-01-18 15:23:14 +0100

    mergin manuel du fichier handler test et du fichier principal
    Ajout des fonctions de calcul d'énergie et de norme euclidienne entre deux points

--- svg_handler.py
+++ svg_handler.py
@@ -1,4 +1,5 @@
 from svgpathtools import svg2paths, paths2svg
+import numpy as np
 
 ###FIchier où on va mettre les fonctions pour ouvrir et renvoyer les svg
 #Req: svgpathtools
@@ -9,24 +10,58 @@ def pointCoord(svgpath):
     Divise en assemblage de chemins les cycles;
      Un chemin est composé de lignes partant d'un point de départ jusqu'à un point d'arrivée
      Puis récupère les coordonnées individuelles du chemin
-
-     Renvoie les coordonnées (complexes) de chaque point du chemin/cycle.
+     Renvoie une liste de liste contenant les coordonnees complexes de chaque point d'un cycle.
       '''
     paths = svg2paths(svgpath)[0]
-    print(f"paths:{paths}")
+    # print(f"paths:{paths}")
 
+    liste_de_point = []
+    nb_path = 0 
     for path_discontinuous in paths:
-        print(f"path discontinuous: {path_discontinuous}")
-        print(f"continuous subpath: {path_discontinuous.continuous_subpaths()}")
+        #print(f"path discontinuous: {path_discontinuous}")
+        #print(f"continuous subpath: {path_discontinuous.continuous_subpaths()}")
         for path in path_discontinuous.continuous_subpaths():
-            print(f'path: {path}')
+            #print(f'path: {path}')
+            liste_de_point.append([])
             for i in range(len(path)):
-                print(path[i][0])
+                # print(path[i][0])
+                liste_de_point[nb_path].append(path[i][0])
+            nb_path += 1
+    return liste_de_point
+
+
+def energyCalc(edge1, edge2):
+    """
+    Prend en entrée deux lignes (type liste) 
+
+    et renvoie l'énergie de 'patch' selon la formuleb pg 8.
+    """
+    res = min(norm2(edge1[0] - edge2[1])+ norm2(edge1[1] - edge2[0]), 
+              norm2(edge1[0] - edge2[0]) + norm2(edge1[1] - edge2[1])) 
+    
+    res -= norm2(edge1[0] - edge1[1]) - norm2(edge2[0]- edge2[1])
+    
+    return res
+
+def norm2(point_a, point_b):
+    """
+    Prend deux points complexes en entrée
+
+    calcule la différence
+
+    Renvoie la distance avec la norme euclidienne.
+    """
+    difference = (point_a.real - point_b.real, point_a.imag - point_b.imag) #différence des points a et b
+    res = np.sqrt(difference[0]**2+difference[1]**2) #distance entre a et b (norme 2)
+    return res
 
 def stitchEdges(A, B):
     """créer un objet Line du package svgpathtools pour lier deux points.
-    Ne renvoie rien."""
+    
+    
+    """
     pass
+
 def cyclesToGraph(paths):
     pass
 
--- test_svg.py
+++ test_svg.py
@@ -1,4 +1,4 @@
-import svg_handler_test as svgh
-
-print(len(svgh.pointCoord("svg_entries\svg\\brain_cycles.svg")))
+import svg_handler as svgh
+print(svgh.pointCoord("svg_entries\svg\simple.svg"))
+print(len(svgh.pointCoord("svg_entries\svg\simple.svg")))
 
