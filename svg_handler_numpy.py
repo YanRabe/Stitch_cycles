e6ed67e7eea902088988b01429bd94b58f96ab08
DindeDouce <samuel.schoirfer2@etu.univ-lorraine.fr>
2024-03-06 10:54:44 +0100

    fin svgh np

--- svg_handler_numpy.py
+++ svg_handler_numpy.py
@@ -19,7 +19,7 @@ def pointCoord(svgpath):
      avec la partie réelle dans la colonne 0 et la partie imaginaire dans la colonne 1.
       '''
     paths = svg2paths(svgpath)[0]
-    array_de_points = np.empty((0,2))
+    array_de_point = np.empty((0,2))
 
     for path_discontinuous in paths:
         
@@ -30,44 +30,73 @@ def pointCoord(svgpath):
                 point_np = np.array([point[1].real,point[1].imag])
                 cycle_de_point = np.vstack((cycle_de_point,point_np))
 
-            array_de_points = np.vstack((array_de_points,cycle_de_point))
+            array_de_point = np.vstack((array_de_point,cycle_de_point))
 
-    return  array_de_points
+    return  array_de_point
+
+
+def pointCoordList(svgpath):
+    '''Récupère en entrée le nom du fichier
+    
+    Divise en assemblage de chemins les cycles;
+     Un chemin est composé de lignes partant d'un point de départ jusqu'à un point d'arrivée
+     Puis récupère les coordonnées individuelles du chemin
+     
+     Renvoie une liste d'array de dimmension N,2 contenant les coordonnees complexes de chaque point du cycle
+     avec la partie réelle dans la colonne 0 et la partie imaginaire dans la colonne 1.
+      '''
+    paths = svg2paths(svgpath)[0]
+    liste_d_array = []
+
+    for path_discontinuous in paths:
+        
+        for path in path_discontinuous.continuous_subpaths():
+
+            cycle_de_point = np.array([path[0][0].real,path[0][0].imag])
+            for point in path[:-1]:
+                point_np = np.array([point[1].real,point[1].imag])
+                cycle_de_point = np.vstack((cycle_de_point,point_np))
+                
+            liste_d_array.append(cycle_de_point)
+
+    return liste_d_array
 
 
 def cyclesToGraph(paths):
     """_summary_
-    recupere le nom du fichier en entree et renvoie 3 listes:
-    -une liste contenant la totalite des points complexes
-    -une liste d'adajcence contenant une liste par point contenant l'indice du point suivant et du point precedent
-    -une liste contenant une liste par cycle, avec l'indice du point de depart et le nombre de points du cycle
+    recupere le nom du fichier en entree et renvoie 3 array:
+    -une array de dimension N,2 ontenant la totalite des points complexes
+    -une array d'adajcence de dimension N,2 contenant l'indice du point suivant et du point precedent
+    -une array de dimension M,2 (avec M le nombre de cycle), avec l'indice du point de depart et le nombre de points du cycle
     """
-    array_de_points = pointCoord(paths) #contient tous les points du cycle
-    liste_d_adjacence =np.empty((0,2)) 
-    liste_indice_depart = np.empty((0,2))  #continent une liste par cycle avec l'indice de depart et le nombre de points du cycle
+    array_de_point_initiale = pointCoordList(paths)
+    array_d_adjacence = np.empty((0,2))
+    array_indice_depart = np.empty((0,2)) #continent une array par cycle avec l'indice de depart et le nombre de points du cycle
+    array_de_point = np.empty((0,2))#contient l'ensemble des points dans une seule array
+    
+    for indice_cycle in range(len(array_de_point_initiale)):
         
-    for indice_cycle in range(len(array_de_points)):
+        array_indice_depart = np.vstack((array_indice_depart,np.array([len(array_de_point),array_de_point_initiale[indice_cycle].shape[0]])))
         
-        liste_indice_depart=np.vstack((liste_indice_depart,np.array(len(array_de_points),len(array_de_points[indice_cycle]))))
+        delta_nb_point = array_de_point.shape[0]
         
-        delta_nb_point = len(array_de_points)
+        array_de_point = np.vstack((array_de_point,array_de_point_initiale[indice_cycle]))
         
-        array_de_points += array_de_points[indice_cycle]
-        
-        for indice_point in range(len(array_de_points[indice_cycle])):
-            if indice_point == len(array_de_points[indice_cycle])-1:
+        for indice_point in range(array_de_point_initiale[indice_cycle].shape[0]):
+            
+            if indice_point == array_de_point_initiale[indice_cycle].shape[0]-1:
                 indice_suivant = 0 
             else:
                 indice_suivant = indice_point + 1
                 
             if indice_point == 0:
-                indice_precedent = len(array_de_points[indice_cycle])-1
+                indice_precedent = array_de_point_initiale[indice_cycle].shape[0]-1
             else:
                 indice_precedent = indice_point -1
                 
-            liste_d_adjacence.append([indice_suivant + delta_nb_point,indice_precedent+delta_nb_point])
+            array_d_adjacence = np.vstack((array_d_adjacence,np.array([indice_suivant + delta_nb_point,indice_precedent+delta_nb_point])))
         
-    return array_de_points,liste_d_adjacence,liste_indice_depart
+    return array_de_point,array_d_adjacence,array_indice_depart
                 
 
 def pathsToSvg(points):
